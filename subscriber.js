78a33936590ab22e19332f0d4c0945c53965baa3
adnanrahic <ado.raha198@gmail.com>
2019-04-01 13:34:32 +0200

    removed filter pattern dynamic setting

--- subscriber.js
+++ subscriber.js
@@ -7,19 +7,13 @@ const roleArn = process.env.roleArn
 const filterName = process.env.filterName
 const retentionDays = process.env.retentionDays
 const shipperFunctionName = process.env.shipperFunctionName
-const filterPattern = process.env.filterPattern
-// const filterPattern = ''
-// const filterPattern = '[...]'
-// const filterPattern = '[..., timestamp, request, event, status_code]'
-// const filterPattern = '[timestamp=*Z, request_id="*-*", event]'
+const filterPattern = ''
 
-const setRetentionPolicy = async (logGroupName) => {
-  const params = {
+const setRetentionPolicy = async (logGroupName) =>
+  cloudWatchLogs.putRetentionPolicy({
     logGroupName: logGroupName,
     retentionInDays: retentionDays
-  }
-  await cloudWatchLogs.putRetentionPolicy(params).promise()
-}
+  }).promise()
 
 const listLogGroups = async (acc, nextToken) => {
   const req = {
